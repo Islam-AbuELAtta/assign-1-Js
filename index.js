dd911860b4ac3ff955a9b507df415f8ee0abbc11
Islam-AbuELAtta <Islam.abuelatta1@gmail.com>
2024-05-31 00:31:20 +0300

    first-commit

---
+++ index.js
@@ -0,0 +1,412 @@
+
+
+                            // Start my new assign js (1) //
+
+/*
+ number (1)
+
+    var x= 5;
+    console.log('x');
+    window.alert('x');
+    document.getElementById("moka").innerHTML=("x") ; 
+*/
+
+
+
+
+/*
+number (2)
+     var x = 9;
+
+     if ( x % 3 ==0  && x % 4 == 0){
+        console.log("output yes");
+     } else{
+        console.log('output no');
+     } 
+*/
+
+
+/*
+number (3)
+
+ var x= 10 ;
+ var y = 7 ;
+
+     if( x > y){
+        console.log("x is greater than y =", x);
+     }
+     else{
+        console.log('y is greater than x =' , y);
+     } 
+*/
+
+
+
+/*
+number (4)     
+    var x= 3;
+
+    if( x > 0){
+        console.log(" positive number");
+    }
+    else if( x< 0){
+        console.log("Negative number");
+    }
+    else{
+        console.log("it's zero ");
+    } 
+*/ 
+
+
+
+ /*
+    number (5)
+
+    var x =  30;
+    var y = 25 ; 
+    var z = 99 ;
+
+
+    if ( x > y && x > z){
+        console.log("max element = " , x);
+            if ( y > z ){
+                console.log(" min element = ", y) ;
+            }
+            else{
+                console.log(" min element =", z);
+            }
+    }
+    else if( y> x && y > z) {
+        console.log(" max element =" , y);
+         if (x > z) {
+            console.log("min element =" , x);
+         } 
+         else{
+            console.log(" min element =" , z);
+         }
+    }
+    else{
+        console.log("max element =" , z);
+        if(x>y){
+            console.log("min element = " , x);
+        }
+        else{
+            console.log(" min element =" , y);
+        }
+    }
+*/ 
+
+
+
+/* 
+    number (6)
+
+    var x =8 ;
+
+    if ( x % 2 == 0){
+        console.log(" it's even "); // زوجى 
+    }
+    else {
+        console.log(" it's odd ");
+    }
+*/ 
+   
+
+
+/* 
+number (8)
+
+ var x = "a"
+ if ( x == "o" || x == "O" ){
+    console.log("vowel");
+ }
+ else  if ( x == "i" || x == "I" ){
+    console.log("vowel");
+ }
+ else  if ( x == "a" || x == "A" ){
+    console.log("vowel");
+ }
+ else  if ( x == "e" || x == "E" ){
+    console.log("vowel");
+ }
+ else  if ( x == "u" || x == "U" ){
+    console.log("vowel");
+ }
+
+ else{
+    console.log("Consonant");
+ }
+ */
+  
+
+
+ /* 
+ number (9)
+
+ var n = 5 ;
+
+ for ( var i=1; i <= n ; i++){
+    console.log(i);
+ }
+
+*/
+
+
+
+ /* 
+
+ number (10)
+
+ var n = 5 ; 
+
+ for (var i = 1 ; i <=12 ; i++){
+    console.log(n*i);
+ }
+
+ */ 
+
+
+ /* 
+ number (11)
+
+
+ var n = 15 ; 
+
+ for ( var i = 1 ; i<=n ; i++){
+    if (i%2 ==0 ) {
+        console.log(i);
+    }
+ }
+
+ */ 
+
+
+
+ /*
+ number (12)
+
+ var x = 4 ;
+ var y = 3 ; 
+ console.log(x**y);
+
+ */ 
+
+
+
+ /* 
+
+ number (12  ~ repeted)
+
+ var n = 95 ;  
+ var y = 76 ;
+ var z = 85 ;
+ var a = 90 ;
+ var b = 89 ;
+
+ var sum =0 ; 
+        sum = (n+y+z+a+b)
+        console.log(sum);
+ var x =0 ;
+        x = (sum)/5 ;
+        console.log(x);
+        console.log(x%100);
+
+ */
+
+
+
+/* 
+    number (13)
+
+ var n =  4; 
+ 
+ if ( n == 1 || n== 3 || n==5 || n== 7 || n==8 || n==10 || n==12){
+    console.log("days in month 31");   
+ }
+
+ else if (n==4 || n==6 || n==9 || n==11){
+    console.log("days in month 30");
+ }
+
+ else if (n==2){
+    console.log("days in month 28");
+ }
+ else{
+    console.log("not a month ");
+ }
+*/ 
+
+
+
+
+/* 
+
+                number (14)
+
+var phy = 10;
+var chem = 100;
+var Bio = 55;
+var math = 40;
+var comp = 100;
+var total ;
+        total = 500; // suppose full mark 100 for each subject .  
+ var resultSum ;
+        resultSum = (phy + chem + Bio + math + comp) ;
+ var userPercent;
+        userPercent = ((resultSum/total)* 100 ) ;
+    if (userPercent >= 90%100 ){
+            console.log("your grade is A");
+    }
+    else if (userPercent >= (80%100)){
+            console.log("your grade is B");
+    }
+    else if (userPercent >= (70%100)){
+            console.log("your grade is C");
+    }
+    else if (userPercent >= (60%100)){
+            console.log("your grade is D");
+    }
+    else if (userPercent >= (40%100)){
+            console.log("your grade is E");
+    }
+    else {
+            console.log("your grade is F");
+    }
+
+*/ 
+    
+
+
+                  // start switch  case .
+
+
+
+
+   /* 
+      case number (15)
+
+    switch (true) {
+        case  true :
+                console.log('31 day');
+            break;
+    }
+
+    */ 
+
+
+
+
+    /*
+
+        case number (16)
+
+var userLetter = 'u' ;
+            switch (userLetter) {
+                case 'a' : case 'A':
+                        console.log('this letter is vowel');
+                    break;
+                case 'i':  case 'I':
+                        console.log('this letter is vowel');
+                    break;
+                case 'o' : case'O':
+                        console.log('this letter is vowel');
+                    break;
+                case 'e' :  case 'E':
+                        console.log('this letter is vowel');
+                    break;
+                case 'u':  case 'U':
+                        console.log('this letter is vowel');
+                    break;
+                default:
+                        console.log(" this letter is conso");
+                    break;
+            }
+
+      */ 
+
+
+
+
+        /* 
+
+            case number (17)
+
+            var  x =  1687; 
+            var  y = 1257 ;
+
+            switch (true) {
+                case  x > y  :
+                    console.log("x is greater than y ");
+                    break;
+                default:
+                    console.log("y is greater than x ");
+                    break;
+            }
+
+            */ 
+
+
+
+
+
+        /* 
+
+            case number (18)
+
+        var z = 30;
+
+        switch (true) {
+            case   (z%2==0) :
+                    console.log(" it's even number ");
+                break;
+            default:
+                console.log(" it's odd number ");
+                break;
+        }
+
+        */ 
+
+
+
+
+
+        /*
+
+            case number (19)
+
+        var x = 55 ; 
+        switch (true) {
+            case x>0:
+                console.log(" it's positive number");
+                break;
+            case x<0:
+                console.log(" it's negative number");
+                break;
+            default:
+                console.log(" it's zero ");
+                break;
+        }
+
+        */
+
+
+
+
+
+
+        /* 
+
+        case number (20)
+ 
+        var x = 5 ;
+        var y = 11; 
+        var sum ;
+
+        sum = (x+y) ;
+        sum+= 
+        console.log(sum);
+
+        */ 
+
+
+                // end of assignment 
